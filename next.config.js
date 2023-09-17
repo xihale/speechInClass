626824184e2985565f6479ea6e561d0cbd7b5535
xihale <xihale.top@qq.com>
2023-09-17 13:46:32 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  "output": "export",
+  "distDir": "dist",
+  "basePath": "/speechInClass",
+}
 
 module.exports = nextConfig
